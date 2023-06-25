791e3edd867bf268c18be3ca503e19507339e293
Giulia Lacerda <giulialms@gmail.com>
2023-06-24 22:56:20 -0300

    formatando texto

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 
 const Body = () => {
   return (
-    <div className="py-4 mx-4 my-4 p-4 bg-background rounded-lg">
-    <p className="text-left md:text-center text-base text-gray-700">
-        Bem-vindo à GadgetZones, a sua loja online de confiança para todas as suas necessidades em celulares! Somos uma empresa fictícia apaixonada por tecnologia, dedicada a oferecer aos nossos clientes a mais ampla variedade de dispositivos móveis de última geração. Com uma equipe de especialistas entusiasmados e experientes, estamos prontos para ajudá-lo a encontrar o telefone perfeito para atender às suas necessidades e estilo de vida.
+    <div className="leading-relaxed text-left py-4 mx-8 my-4 px-9">
+        
+    <div className="flex flex-wrap justify-center">
+        <div className="max-w-lg">
+    <p className="text-start text-base text-gray-700">
+        Bem-vindo à GadgetZones, a sua loja online de confiança para todas as suas necessidades em celulares! Somos uma empresa apaixonada por tecnologia, dedicada a oferecer aos nossos clientes a mais ampla variedade de dispositivos móveis de última geração. Com uma equipe de especialistas entusiasmados e experientes, estamos prontos para ajudá-lo a encontrar o telefone perfeito para atender às suas necessidades e estilo de vida.
     </p>
-    <p className="py-4 text-left md:text-center text-base text-gray-700">
+    <p className="py-4 text-left text-base text-gray-700">
         Na GadgetZones, priorizamos a qualidade, a inovação e a satisfação do cliente. Trabalhamos em estreita colaboração com os principais fabricantes de celulares do mercado, garantindo que ofereçamos apenas os produtos mais avançados e confiáveis. Navegue em nosso catálogo e descubra uma ampla seleção de smartphones de marcas renomadas, com recursos avançados, câmeras poderosas e desempenho excepcional.
     </p>
-    <p className="py-4 text-left md:text-center text-base text-gray-700">
-        Além disso, estamos comprometidos em fornecer uma experiência de compra conveniente e segura. Nosso site foi projetado para facilitar a busca e a compra dos produtos que você deseja, com informações detalhadas e claras sobre cada dispositivo. Garantimos envio rápido e seguro, além de oferecer um atendimento ao cliente excepcional, pronto para responder a todas as suas dúvidas e preocupações.
- </p>
-    <p className="py-4 text-left md:text-center text-base text-gray-700">
-    Na GadgetZones, acreditamos que a tecnologia pode transformar vidas e queremos ajudá-lo a encontrar o celular perfeito para acompanhá-lo em todas as suas aventuras. Explore nossa loja virtual hoje mesmo e desfrute da experiência única de adquirir seu novo companheiro tecnológico na GadgetZones!
+    </div>
+    <div className="max-w-lg">
+    <p className="py-4 text-left text-base text-gray-700">
+        Na GadgetZones, priorizamos a qualidade, a inovação e a satisfação do cliente. Trabalhamos em estreita colaboração com os principais fabricantes de celulares do mercado, garantindo que ofereçamos apenas os produtos mais avançados e confiáveis. Navegue em nosso catálogo e descubra uma ampla seleção de smartphones de marcas renomadas, com recursos avançados, câmeras poderosas e desempenho excepcional.
     </p>
+    </div>
+    </div>
     </div>
   );
 };
